96b3111a6383bb1f1cf65c357aaca6c4a9b3ce3a
Oc9aN <59911975+Oc9aN@users.noreply.github.com>
2024-08-23 14:47:38 +0900

    Tutorial 13
    * Timer + Sprite

--- Project2/applicationclass.h
+++ Project2/applicationclass.h
@@ -14,6 +14,10 @@
 // MY CLASS INCLUDE //
 //////////////////////
 #include "d3dclass.h"
+#include "cameraclass.h"
+#include "textureshaderclass.h"
+#include "spriteclass.h"
+#include "timerclass.h"
 
 /////////////
 // GLOBALS //
@@ -42,6 +46,10 @@ private:
 
 private:
 	D3DClass* m_Direct3D;
+	CameraClass* m_Camera;
+	TextureShaderClass* m_TextureShader;
+	SpriteClass* m_Sprite;
+	TimerClass* m_Timer;
 };
 
 #endif
--- Project2/d3dclass.h
+++ Project2/d3dclass.h
@@ -43,6 +43,9 @@ public:
     void SetBackBufferRenderTarget();
     void ResetViewport();
 
+    void TurnZBufferOn();
+    void TurnZBufferOff();
+
 private:
     bool m_vsync_enabled;
     int m_videoCardMemory;
@@ -59,6 +62,7 @@ private:
     XMMATRIX m_worldMatrix;
     XMMATRIX m_orthoMatrix;
     D3D11_VIEWPORT m_viewport;
+    ID3D11DepthStencilState* m_depthDisabledStencilState;
 };
 
 #endif
